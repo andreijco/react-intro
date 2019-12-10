8a66d0aefd492c33f76bf27aaac26c17e2268a18
andr3wll <andrew.limaru@gmail.com>
2019-12-10 11:52:48 +1100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="col-md-6 col-lg-4 d-flex align-items-stretch">
+        <div className="card mb-3">
+          <img className="card-img-top" src={this.props.img} alt={this.props.imgalt} />
+          <div className="card-body">
+            <h4 className="card-title">{this.props.productName}</h4>
+            Price: <strong>{this.props.price}</strong>
+            <p className="card-text">{this.props.desc}</p>
+            <a href="#" className="btn btn-primary">Buy</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class CardContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <Card key='1' img="img/strings.png" alt="strings" productName="Strings" price='100.0' desc="A very authentic and beautiful instrument!!" />
+        <Card key='2' img="img/redguitar.jpeg" alt="redg" productName="Red Guitar" price='299.0' desc="A really cool red guitar that can produce super cool music!!" />
+      </div>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<CardContainer/>,document.getElementById('root'));
